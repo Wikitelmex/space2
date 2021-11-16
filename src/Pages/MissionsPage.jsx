bb92e577936e841bd5574f6e863e8845b95b43ce
Alex Castillo <wikitelmex@gmail.com>
2021-11-16 09:09:42 -0700

    local linters
    - fix local react linters

--- src/Pages/MissionsPage.jsx
+++ src/Pages/MissionsPage.jsx
@@ -1,49 +1,64 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { fetchMissions } from '../redux';
 
 const MissionsPage = ({ fetchMissions, missionData }) => {
+  useEffect(() => {
+    fetchMissions();
+  }, []);
 
-    useEffect(() => {
-        fetchMissions();
-    }, []);
+  if (missionData.loading) {
+    return <div>Loading...</div>;
+  }
 
-    return missionData.loading ?
-        (
-            <div>Loading...</div>
-        ) : missionData.error ?
-            (
-                <div> error {missionData.error}</div>
-            ) : (
-                <div>
-                    <h1>Missions</h1>
-                    <ul>
-                        {
-                            missionData &&
-                            missionData.missions &&
-                            missionData.missions.map(mission => (
-                                <li key={mission.id}>
-                                    <h2>{mission.name}</h2>
-                                    <p>{mission.description}</p>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            );
+  if (missionData.error) {
+    return (
+      <div>
+        Error!
+        {missionData.error}
+      </div>
+    );
+  }
 
-
-
-
-
+  return (
+    <div>
+      <h1>Missions</h1>
+      <ul>
+        {
+          missionData
+          && missionData.missions
+          && missionData.missions.map((mission) => (
+            <li key={mission.id}>
+              <h2>{mission.name}</h2>
+              <p>{mission.description}</p>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-    missionData: state.missions
+MissionsPage.propTypes = {
+  fetchMissions: PropTypes.func.isRequired,
+  missionData: PropTypes.shape({
+    loading: PropTypes.bool,
+    error: PropTypes.string,
+    missions: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+    })),
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  missionData: state.missions,
 });
 
-const mapDispatchToProps = dispatch => ({
-    fetchMissions: () => dispatch(fetchMissions())
+const mapDispatchToProps = (dispatch) => ({
+  fetchMissions: () => dispatch(fetchMissions()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MissionsPage);
--- src/Pages/RocketsPage.jsx
+++ src/Pages/RocketsPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const RocketsPage = () => {
-    const x = "rockets page under construction";
-    return (
-        <div>
-            <p>{x}</p>
-        </div>
-    );
-}
+  const x = 'rockets page under construction';
+  return (
+    <div>
+      <p>{x}</p>
+    </div>
+  );
+};
 
 export default RocketsPage;
